1c236f577d5e99b8b91c907de2578f97c6729d88
Alex2jer <30531891+Alex2jer@users.noreply.github.com>
2017-07-28 18:47:05 +0200

    Update client.lua

--- resources/[citizenv]/es_bank/client.lua
+++ resources/[citizenv]/es_bank/client.lua
@@ -55,7 +55,7 @@ end)
 RegisterNetEvent('es_bank:toofarlocal')
 AddEventHandler('es_bank:toofarlocal', function(robb)
 	robbing = false
-	TriggerEvent('chatMessage', 'SYSTEM', {255, 0, 0}, "The robbery was cancelled, you will receive nothing.")
+	TriggerEvent('chatMessage', 'SYSTEM', {255, 0, 0}, "Le braquage est annule vous recevez rien.")
 	robbingName = ""
 	secondsRemaining = 0
 	incircle = false
@@ -65,7 +65,7 @@ end)
 RegisterNetEvent('es_bank:robberycomplete')
 AddEventHandler('es_bank:robberycomplete', function(robb)
 	robbing = false
-	TriggerEvent('chatMessage', 'SYSTEM', {255, 0, 0}, "Robbery done, you received: ^2" .. banks[bank].reward)
+	TriggerEvent('chatMessage', 'SYSTEM', {255, 0, 0}, "Braquage reussi vous recevez: ^2" .. banks[bank].reward)
 	bank = ""
 	secondsRemaining = 0
 	incircle = false
